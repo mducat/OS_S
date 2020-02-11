60cf283683c2e7debf9eb02a7c217b57bcbc5958
Loock Florent <florent.loock@epitech.eu>
2020-02-11 13:02:51 -0500

    add (mvwrite && write && GOODENOUGHT)

--- src/kernel.c
+++ src/kernel.c
@@ -1,22 +1,53 @@
 /*
 ** EPITECH PROJECT, 2019
-** SHIELD_OS
+** OSS
 ** File description:
 ** Kernel main file
 */
 
-void write_screen(char *str)
+#include "kernel.h"
+//25 80 -> window size
+void mvprint(int x, int y, char *str, char color)
 {
     char *screen = (void *) 0xb8000;
 
-    for (int i = 0; str[i] != '\0'; i++){
-        screen[i * 2 + 1] = 0x09; // Color 0x9 = light blue
-        screen[i * 2] = str[i];
+    int str_p = 0;
+    int screen_p = (x+y*80);
+    for (; str[str_p]; screen_p++, str_p++){
+        if (str[str_p] == '\n'){
+            screen_p += 80;
+            screen_p -= screen_p % 80;
+            str_p += 1;
+        }
+        screen[screen_p * 2] = str[str_p];
+        screen[screen_p * 2 + 1] = color; // Color 0x9 = light blue
+    }
+}
+
+
+void write_screen(char *str)
+{
+    char a[2];
+    static int actual_x = 0;
+    static int actual_y = 0;
+
+    a[1] = 0;
+    for (int i = 0; str[i]; i++) {
+        if (str[i] == '\n') {
+            actual_y = (actual_y > 24) ? 0 : actual_y + 1;
+            actual_x = 0;
+            i++;
+        }
+        a[0] = str[i];
+        mvprint(actual_x, actual_y, &a, 0x07);
+        (actual_x > 79) ? actual_x = 0, actual_y++ : actual_x + 1;
     }
 }
 
 void kernel_main(void)
 {
-    write_screen("Hello, world !\n Re-hello !");
-    write_screen("Test !");
+    mvprint(0, 0, GOODENOUGHT, 0x09);
+    /*
+    write_screen(GOODENOUGHT);
+    write_screen("florent\nest\nobese\nquand\nil\nparle de david");*/
 }
